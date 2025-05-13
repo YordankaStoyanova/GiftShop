00f6de8c528bc1ededa09db77fb7add3d7c2fcb2
inachanova1606 <119493933+inachanova1606@users.noreply.github.com>
2025-05-13 09:34:02 +0300

    class Feedback

--- WebAPI/BusinessLayer/Product.cs
+++ WebAPI/BusinessLayer/Product.cs
@@ -13,7 +13,8 @@ namespace BusinessLayer
         public int Id { get; set; }
 
         [Required]
-        [Range(0, 100, ErrorMessage = "Brand must be in (0;100]")]
+        [MaxLength(50, ErrorMessage = "Brand must not be more than 50 symbols!")]
+        [MinLength(2, ErrorMessage = "Brand must be at least 2 symbols!")]
         public string Brand { get; set; }
 
         [Required]
